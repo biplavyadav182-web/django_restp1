6e25b0a12bed4a5622ad2f024795aa5af023072c
Biplav Yadav <biplavyadav182@gmail.com>
2026-03-19 16:04:03 +0545

    Second commit -DRF project

--- main_app/views.py
+++ main_app/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from .serializers import TransactionsSerializer
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from django.db.models import Sum
+
 
 @api_view(["GET", "POST"])
 def transaction_function(request):
@@ -14,17 +16,30 @@ def transaction_function(request):
 
 class TransactionAPI(APIView):
     def get(self, request):
-        queryset = Transaction.objects.all()
+        queryset = Transaction.objects.all().order_by('-id')
         serializers = TransactionsSerializer(queryset, many =True)
+        total = queryset.aggregate(total = Sum('amount')) ['total'] or 0
         return Response({
-        "data" : serializers.data
+        "data" : serializers.data,
+        "total": total
     })
         
     def post(self, request):
+        data = request.data
+        serializers = TransactionsSerializer(data=data)
+        
+        if not serializers.is_valid():
+            return Response({
+                "message":"data not saved",
+                "errors":serializers.errors, 
+            })
+        serializers.save()
         return Response({
-            "message":"this is a post method."
-        } 
-        )
+            "message":"data saved",
+            "data": serializers.data
+        })
+        
+        
             
         
     
@@ -33,9 +48,46 @@ class TransactionAPI(APIView):
             "message":"this is a put method."
         })
         
+        
+        
+        
     def patch(self, request):
+        
+        data = request.data
+        if not data.get('id'):
+            return Response({
+                "message":"data not updated",
+                "errors":"id is required",
+            })
+            
+        transaction = Transaction.objects.get(id=data.get('id'))
+        serializers = TransactionsSerializer(transaction, data=data, partial=True)
+        
+        if not serializers.is_valid():
+            return Response({
+                "message":"wrong xa",
+                "erros":"id is required",
+            })
+        serializers.save()
         return Response({
-            "message":"this is a patch method."
+            "message":"this is a patch method.",
+            "data":serializers.data
+        })
+        
+        
+    def delete(self, request):
+        data = request.data
+        if not data.get('id'):
+            return Response({
+                "message":"data not updated",
+                "errors":"id is required",
+            })
+            
+        transaction = Transaction.objects.get(id = data.get('id')).delete()
+        
+        return Response({
+            "message":"data deleted",
+            "data" : {}
         })
         
         
